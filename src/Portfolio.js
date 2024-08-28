a666da5884458f03c8b16ac8632df329c053c78c
Natesh Kumar <74709189+NATESHKUMAR485@users.noreply.github.com>
2024-08-29 00:25:50 +0500

    edu update

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -6,7 +6,8 @@ const Portfolio = () => {
   return (
     <div className="portfolio-container">
       <img src={profilePic} alt="Profile" className="profile-pic" />
-      <h1 className="name">Jeetu</h1>
+      <h1 className="name">Jetender Kumar</h1>
+      <p className="title">Web Developer</p>
       <p className="description">
         I am a passionate web developer with experience in creating dynamic web applications using modern technologies.
       </p>
@@ -15,8 +16,8 @@ const Portfolio = () => {
       <div className="education">
         <h2>Education</h2>
         <ul>
-          <li><strong>Bachelor of Science in Computer Science</strong> - IBA Karachi, 2028</li>
-          <li><strong>Relevant Coursework:</strong> Web Development, Data Structures, Algorithms</li>
+          <li><strong>Intermediate in Computer Science</strong> - Suffa Institue Karachi, 2025</li>
+          <li><strong>Relevant Coursework:</strong> Mathematics, Programming, Web development, graphic designing...</li>
         </ul>
       </div>
 
@@ -24,6 +25,9 @@ const Portfolio = () => {
         <h2>Connect with Me</h2>
         <a href="https://www.facebook.com/profile.php?id=61555855944475&mibextid=ZbWKwL" target="_blank" rel="noopener noreferrer">Facebook</a>
         <a href="https://www.instagram.com/kumarjeteender?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank" rel="noopener noreferrer">Instagram</a>
+
+        
+
       </div>
 
 
